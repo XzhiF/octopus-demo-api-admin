90df9f3c900d18fd37192e849070649b8ff69a92
xiezhifeng <124956282@qq.com>
2026-05-22 02:20:52 +0800

    first commit

---
+++ src/test/java/com/octopus/demo/controller/DemoControllerTest.java
@@ -0,0 +1,24 @@
+package com.octopus.demo.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(DemoController.class)
+class DemoControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void demoReturnsHelloMessage() throws Exception {
+        mockMvc.perform(get("/demo"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value("hello from octopus-demo-api-admin"));
+    }
+}
